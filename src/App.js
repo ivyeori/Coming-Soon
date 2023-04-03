80d13d6441fe8fb939a7ea9ea649863888f78f7b
Ivyeori <mail@getalex.info>
2023-04-04 00:18:06 +0200

    yes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+function Coming() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="relative min-h-screen flex flex-col justify-center items-center bg-hero bg-cover">
+    
+    <div class="relative z-10 py-6 space-y-16 lg:space-y-32 text-gray-900">
+        <div class="text-center space-y-10">
+    <h3 class="font-light text-xl uppercase tracking-wider">Coming soon</h3>
+    <h1 class="text-7xl lg:text-9xl font-extrabold">We’re blowing up</h1>
+    <p class="text-xl lg:text-2xl tracking-wide mx-10 lg:max-w-xl lg:mx-auto">We`re under construction. Check back for an update soon. Stay in touch!</p>
     </div>
-  );
+    </div>
+    </div>
+    
+   
+  )
 }
 
-export default App;
+export default Coming
